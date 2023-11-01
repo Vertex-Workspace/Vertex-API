d702455c6576861c3866bc09a5b9214151d57a1f
miguelbertoldi <miguelbertoldi1@gmail.com>
2023-10-31 21:26:20 -0300

    user entity

--- src/main/java/com/vertex/vertex/permission/model/entity/UserP.java
+++ src/main/java/com/vertex/vertex/permission/model/entity/UserP.java
@@ -6,16 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.awt.*;
-
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class UserP {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Lob
-    private Image image;
+
+    
+//    @Lob
+//    private Image image;
 }
---
+++ src/main/java/com/vertex/vertex/team/repository/TeamRepository.java
@@ -0,0 +1,8 @@
+package com.vertex.vertex.team.repository;
+
+import com.vertex.vertex.team.model.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository
+        extends JpaRepository<Team, Long> {
+}
