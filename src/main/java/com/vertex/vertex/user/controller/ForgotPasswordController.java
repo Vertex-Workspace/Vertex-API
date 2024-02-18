d627012b150761c237e5f1e29e7b7660eea88924
KaiqueFLH <kaiquef.homem@gmail.com>
2024-02-18 17:51:02 -0300

    update. the basic is done

---
+++ src/main/java/com/vertex/vertex/user/controller/ForgotPasswordController.java
@@ -0,0 +1,30 @@
+package com.vertex.vertex.user.controller;
+
+import com.vertex.vertex.user.service.ForgotPasswordService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+@RestController
+@RequestMapping("/forgotPassword")
+@AllArgsConstructor
+@CrossOrigin
+public class ForgotPasswordController {
+
+    private final ForgotPasswordService forgotPasswordService;
+    @GetMapping("/{emailTo}")
+    public ResponseEntity<Long> sendCodeToEmail(@PathVariable String emailTo){
+        try {
+            return new ResponseEntity<>(forgotPasswordService.sendCodeToEmail(emailTo), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+}
