f7a39f4a9c3ba1207b01cb878f8459ac4f312ae7
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2024-03-05 21:42:31 -0300

    everything is working, the future starts here

--- src/main/java/com/vertex/vertex/team/model/entity/Team.java
+++ src/main/java/com/vertex/vertex/team/model/entity/Team.java
@@ -18,7 +18,6 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Team {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
