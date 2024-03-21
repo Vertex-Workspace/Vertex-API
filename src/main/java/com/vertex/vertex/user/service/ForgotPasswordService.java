bc1fbc8fb7461f743dccc6b692ecda7d6e84fa2d
KaiqueFLH <kaiquef.homem@gmail.com>
2024-03-21 09:30:04 -0300

    update. code to email has a image now

--- src/main/java/com/vertex/vertex/user/service/ForgotPasswordService.java
+++ src/main/java/com/vertex/vertex/user/service/ForgotPasswordService.java
@@ -1,82 +1,140 @@
 package com.vertex.vertex.user.service;
 
+
+import org.apache.commons.io.IOUtils;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Service;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import java.io.UnsupportedEncodingException;
+import javax.mail.internet.MimeMultipart;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Properties;
 
 @Service
 public class ForgotPasswordService {
 
     public Long sendCodeToEmail(String emailTo) {
-        Properties properties = System.getProperties();
+        Properties properties = new Properties();
 
-        // define os dados básicos
         String host = "smtp.gmail.com";
         String email = "vertex.workspacee@gmail.com";
         String password = "sryx rozm wixy iool";
 
-        //liga os protocolo tudo
+        // Configuração das propriedades do servidor SMTP
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", "465");
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
-        //cria a sessão
+        // Cria a sessão com autenticação
         Session session = Session.getDefaultInstance(properties, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(email, password);
             }
         });
 
-        //mostra onde tá os erros
+        // Ativa o modo de depuração para visualizar possíveis erros
         session.setDebug(true);
 
+        // Geração do código de recuperação
         Long code = generateCode();
         System.out.println(code);
 
         try {
+            // Criação da mensagem de e-mail
             MimeMessage message = new MimeMessage(session);
-            // Configurando quem envia
             message.setFrom(new InternetAddress(email, "VERTEX: Gerencie seu tempo de ponta a ponta!"));
-            // Configurando quem recebe
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(emailTo));
-            // Configurando o assunto
             message.setSubject("O código de recuperação da sua conta VERTEX é: " + code + "\n");
 
-            // Construindo o conteúdo HTML do e-mail
-            String htmlContent = "<html><body style='background-color: #f6f8fa;'>"+
-            "<div style='margin: 0 auto; max-width: 600px; padding: 20px; text-align: center;'>"+
-//            "<img src='https://github.com/Vertex-Workspace/Vertex/blob/main/src/assets/vertex-logo.png?raw=true' alt='VERTEX LOGO' style='margin-bottom: 20px;'>"+
-            "<div style='background-color: #fff; border-radius: 6px; padding: 40px;'>"+
-            "<h2 style='color: #092C4C; font-weight: 500;'>Código de Recuperação</h2>"+
-            "<p style='font-size: 16px;'>Seu código de recuperação é:</p>"+
-            "<h1 style='font-size: 36px; color: #092C4C; margin-top: 10px; margin-bottom: 30px;'>" + code + "</h1>"+
-            "<p style='font-size: 14px; color: #586069;'>Se você não solicitou esta redefinição de senha, você pode ignorar este email.</p>"+
-            "</div>"+
-            "<p style='font-size: 12px; color: #586069; margin-top: 20px;'>Este é um email automático. Por favor, não responda.</p>"+
-            "</div>"+
-            "</body></html>";
+            // Criação do conteúdo HTML do e-mail
+            MimeMultipart multipart = new MimeMultipart();
 
-            // Configurando o conteúdo do e-mail como HTML
-            message.setContent(htmlContent, "text/html; charset=utf-8");
+            // Texto do e-mail
+            MimeBodyPart textPart = new MimeBodyPart();
+            String htmlContent = "<html><body style='background-color: #f6f8fa;'>" +
+                    "<div style='margin: 0 auto; max-width: 600px; padding: 20px; text-align: center;'>" +
+                    "<img style='width: 150px; height: 140px; margin-bottom: 20px;' src='cid:logo' alt='VERTEX LOGO' >" +
+                    "<div style='background-color: #fff; border-radius: 6px; padding: 40px;'>" +
+                    "<h2 style='color: #092C4C; font-weight: 500;'>Código de Recuperação</h2>" +
+                    "<p style='font-size: 16px;'>Seu código de recuperação é:</p>" +
+                    "<h1 style='font-size: 36px; color: #092C4C; margin-top: 10px; margin-bottom: 30px;'>" + code + "</h1>" +
+                    "<p style='font-size: 14px; color: #586069;'>Se você não solicitou esta redefinição de senha, você pode ignorar este email.</p>" +
+                    "</div>" +
+                    "<p style='font-size: 12px; color: #586069; margin-top: 20px;'>Este é um email automático. Por favor, não responda.</p>" +
+                    "</div>" +
+                    "</body></html>";
+            textPart.setContent(htmlContent, "text/html; charset=utf-8");
+            multipart.addBodyPart(textPart);
 
-            System.out.println("enviando...");
+            // Anexo da imagem da empresa em base64
+            MimeBodyPart imagePart = new MimeBodyPart();
+            String imageData = getImageData();
+            imagePart.setDataHandler(new DataHandler(new ByteArrayDataSource(imageData, "image/png")));
+            imagePart.setHeader("Content-ID", "<logo>");
+            imagePart.setDisposition(MimeBodyPart.INLINE);
+            multipart.addBodyPart(imagePart);
+
+            // Define o conteúdo do e-mail
+            message.setContent(multipart);
+
+            // Envio do e-mail
             Transport.send(message);
             System.out.println("Email enviado com sucesso...");
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
+        } catch (MessagingException | IOException e) {
+            e.printStackTrace();
         }
 
         return code;
     }
 
-    public Long generateCode() {
+    private Long generateCode() {
         return (long) (Math.random() * 1000000);
+    }
+
+    private String getImageData() throws IOException {
+        // Ler a imagem e converter para base64
+        InputStream inputStream = getClass().getResourceAsStream("/vertex-logo.png");
+        byte[] imageData = IOUtils.toByteArray(inputStream);
+        return Base64.encodeBase64String(imageData);
+    }
+
+    // Classe auxiliar para criar um DataSource a partir de uma string de dados
+    static class ByteArrayDataSource implements DataSource {
+        private final byte[] data;
+        private final String type;
+
+        public ByteArrayDataSource(String data, String type) {
+            this.data = Base64.decodeBase64(data);
+            this.type = type;
+        }
+
+        @Override
+        public InputStream getInputStream() {
+            return new ByteArrayInputStream(data);
+        }
+
+        @Override
+        public OutputStream getOutputStream() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String getContentType() {
+            return type;
+        }
+
+        @Override
+        public String getName() {
+            return "ByteArrayDataSource";
+        }
     }
 }
